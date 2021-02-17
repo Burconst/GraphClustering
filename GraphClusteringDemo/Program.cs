7737f25e2e2b3afd2dfa51b9371ce4807855d74e
Konstantin <36280804+Burconst@users.noreply.github.com>
2021-02-17 14:25:02 +0300

    Refactoring

--- GraphClusteringDemo/Program.cs
+++ GraphClusteringDemo/Program.cs
@@ -4,16 +4,9 @@ namespace GraphClusteringDemo
 {
     class Program
     {
- 
-        static void ParseArgs(string[] args) 
-        {
-            
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine(args.Length);
-            ParseArgs(args);
+
         }
     }
 }
